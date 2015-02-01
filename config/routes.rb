8083b682239962b2940204866054d3c48c586580
MarcusHogue <marcus@hogue.me>
2015-01-31 23:27:18 -0600

    Explicitly defined follows post route

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
     root to: "users#index"
 
     get "/createuser", to: "users#create"
+    post "/follows", to: "follows#create"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
